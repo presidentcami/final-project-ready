f8ba1c02454fb73b994233e4e1ee7c0704a5638a
Camille Williams <camiwills325@gmail.com>
2023-05-08 17:05:34 -0400

    Add router for login and profile page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Profile from './components/Profile'
 import './index.css'
 const DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN
 const CLIENTID = import.meta.env.VITE_AUTH0_CLIENT_ID
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='user-profile' element={<Profile />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -17,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       }}
     >
       <App />
+      <RouterProvider router={router} />
     </Auth0Provider>
   </React.StrictMode>,
 )
--- client/src/routes/Navbar.jsx
+++ client/src/routes/Navbar.jsx
@@ -3,10 +3,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Logo from '../assets/BlueTechtonicaWord.png'
 import { useAuth0 } from '@auth0/auth0-react'
+import { Link } from 'react-router-dom';
 
 
 function MyNavBar(props) {
-  const { loginWithRedirect, logout } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   return (
     <>
@@ -23,10 +24,10 @@ function MyNavBar(props) {
           <Nav.Link >Your Link</Nav.Link>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <button onClick={() => loginWithRedirect()}>Log In</button>;
-            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+            {!isAuthenticated ? <button onClick={() => loginWithRedirect()}>Log In</button> : <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
               Log Out
-            </button>
+            </button>}
+
             {/* <Navbar.Text>
             Signed in as: <a href="#login">Cristina Rodriguez</a>
           </Navbar.Text> */}
