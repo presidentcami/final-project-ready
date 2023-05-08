389417c072e1e0d22bd76a6ccd55a49a80ed3216
Camille Williams <camiwills325@gmail.com>
2023-05-08 16:57:35 -0400

    Move navbar to routes folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavBar from './components/Navbar'
+import MyNavBar from './routes/Navbar'
 import ListStudents from './components/ListStudents'
 
 
