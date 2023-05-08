30de04f81f4e5eb1272e86fcce11b45116108e42
Camille Williams <camiwills325@gmail.com>
2023-05-08 16:53:53 -0400

    Add login and logout button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,31 +2,37 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Logo from '../assets/BlueTechtonicaWord.png'
+import { useAuth0 } from '@auth0/auth0-react'
 
 
 function MyNavBar(props) {
+  const { loginWithRedirect, logout } = useAuth0();
 
   return (
     <>
-    <Navbar data-testid="navbar" bg="dark" variant="dark" sticky="top">
-      <Container>
-        <Navbar.Brand href="/">
-        <img
+      <Navbar data-testid="navbar" bg="dark" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
               src={Logo}
               height="30"
               className="d-lg-inline-block"
               alt="React Bootstrap logo"
             />
-        </Navbar.Brand>
-        <Nav.Link >Your Link</Nav.Link>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
+          </Navbar.Brand>
+          <Nav.Link >Your Link</Nav.Link>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <button onClick={() => loginWithRedirect()}>Log In</button>;
+            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+              Log Out
+            </button>
+            {/* <Navbar.Text>
             Signed in as: <a href="#login">Cristina Rodriguez</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Text> */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
