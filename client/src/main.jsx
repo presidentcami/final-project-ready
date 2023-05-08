7deff75c1b01788e43fc76abdda9201af39447e8
Camille Williams <camiwills325@gmail.com>
2023-05-08 16:45:22 -0400

    Add domain and client id to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App'
 import './index.css'
+const DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN
+const CLIENTID = import.meta.env.VITE_AUTH0_CLIENT_ID
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider>
+    <Auth0Provider
+    domain={DOMAIN}
+    clientId={CLIENTID}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
       <App />
     </Auth0Provider>
   </React.StrictMode>,
