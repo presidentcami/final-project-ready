7fb4f139b2de964935b1645cec818649dac4d29b
Camille Williams <camiwills325@gmail.com>
2023-05-13 12:57:20 -0400

    Add a loading page for when the page is loading after authentication

---
+++ client/src/components/PageLoader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageLoader = () => {
+    const loadingImg = "https://cdn.auth0.com/blog/hello-auth0/loader.svg";
+
+    return (
+        <div className="loader">
+            <img src={loadingImg} alt="Loading..." />
+        </div>
+    );
+};
+
+export default PageLoader;
