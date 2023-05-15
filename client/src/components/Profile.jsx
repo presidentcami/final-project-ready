403d93efc98cb8006de3534a5345ea59e9146af4
Camille Williams <camiwills325@gmail.com>
2023-05-15 17:09:25 -0400

    Remove Outlet from profile/dashboard doc

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -7,12 +7,13 @@ import { Outlet } from "react-router-dom";
 
 const Profile = ({ user, trips, setTrips }) => {
 
+    if (!user) return null;
     const { user_id, user_first_name, user_last_name, user_email } = user[0]
     
-    // console.log("profile", user_id)
+    // console.log("profile", user)
     const { isLoading } = useAuth0();
 
-const loadTrips = () => {
+    const loadTrips = () => {
         // A function to fetch the list of students that will be load anytime that list change
         fetch(`http://localhost:8080/trips/${user_id}`)
             .then((response) => response.json())
@@ -31,19 +32,14 @@ const loadTrips = () => {
         return <div>Loading ...</div>;
     }
 
-    return (
+    return user && ( 
             <div className="dashboard">
-                <div>
-                    {trips && <SideBar trips={trips} />}
-                </div>
-                {/* <img src={user.picture} alt={user.name} /> */}
                 <div className="profile">
                 <h2>{user_first_name} {user_last_name}</h2>
                 <p>Welcome to your homepage, user at {user_email}</p>
                 {/* <AllTrips trips={trips} /> */}
                 <AddTrip user={user} setTrips={setTrips} />
                 </div>
-                <Outlet />
             </div>
         
     );
