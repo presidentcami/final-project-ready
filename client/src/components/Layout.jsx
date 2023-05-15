729d8322b842c7808f13fb67943322c57d5f39a3
Camille Williams <camiwills325@gmail.com>
2023-05-15 17:07:55 -0400

    Set up layout so navbar and sidebar are a part of webpage based on logged-in status

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import MyNavBar from '../routes/Navbar'
+import NotLoggedInLandingPage from './NotLoggedInLandingPage'
+import SideBar from './SideBar'
+import { Outlet } from 'react-router-dom'
+
+const Layout = ({ setUser, user, trips, setTrips }) => {
+  return (
+        <div>
+          <MyNavBar setUser={setUser} user={user}/>
+
+            {!!user ? (
+            <div style={{ flex: true }}>
+              <SideBar trips={trips} setTrips={setTrips} />
+            <Outlet />
+        </div>
+    ) : <NotLoggedInLandingPage />}
+    </div >
+  )
+}
+
+export default Layout
