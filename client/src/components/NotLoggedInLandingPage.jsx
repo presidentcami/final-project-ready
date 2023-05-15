c158240c9b13375836c807df4d92b4baa3397c46
Camille Williams <camiwills325@gmail.com>
2023-05-15 17:08:26 -0400

    Update name from homepage to not logged in landing page

---
+++ client/src/components/NotLoggedInLandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotLoggedInLandingPage = () => {
+
+
+    return (
+        <div>
+            <h2>Log in to get Ready!</h2>
+        </div>
+    )
+
+}
+
+export default NotLoggedInLandingPage;
