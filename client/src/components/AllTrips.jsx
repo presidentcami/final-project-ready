e0b64bdb69f4fca6e704ce8937bb6f59cf01e789
Camille Williams <camiwills325@gmail.com>
2023-05-16 15:43:58 -0400

    Add get request for trips

--- client/src/components/AllTrips.jsx
+++ client/src/components/AllTrips.jsx
@@ -1,27 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 
-export default function AllTrips({ trips }) {
+export default function AllTrips({ user, trips, setTrips }) {
 
-    const [show, setShow] = useState(false);
+  if (!user) return null;
+  const { user_id } = user[0]
+
+  const loadTrips = () => {
+    // A function to fetch the list of students that will be load anytime that list change
+    fetch(`http://localhost:8080/trips/${user_id}`)
+      .then((response) => response.json())
+      .then((trips) => {
+        // console.log(trips)
+        setTrips(trips);
+      });
+  }
+
+  useEffect(() => {
+    loadTrips();
+  }, []);
+
     console.log("trips", trips)
 
-    const handleClose = () => setShow(current => !current);
-    const handleShow = () => {
-        // console.log(initialValue)
-        setShow(!show)
-
-    };
   return (
     <div> 
         <div className='profile'>
               {trips.map((trip) => {
                 return (
                 <li key={trip.trip_id}>
-                {trip.trip_name}
-                {trip.location}
+                <Link to={`${trip.trip_id}`}>{trip.trip_name}</Link>
+                {/* {trip.location}
                 {trip.trip_description}
-                {trip.trip_start_date} - {trip.trip_end_date}
+                {trip.trip_start_date} - {trip.trip_end_date} */}
                 
                 </li>)
                 })
