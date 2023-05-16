548e53b144fb6ad56ddae7c41e34bc90c595991f
Camille Williams <camiwills325@gmail.com>
2023-05-16 16:36:12 -0400

    Add get request for trip details on front end

--- client/src/components/OneTrip.jsx
+++ client/src/components/OneTrip.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
 
 const OneTrip = () => {
+  const { trip_id } = useParams();
+  const [tripDetails, setTripDetails] = useState(null)
+  console.log(trip_id)
+
+  const loadTripDetails = () => {
+    // A function to fetch the list of students that will be load anytime that list change
+    fetch(`http://localhost:8080/tripdetails/${trip_id}`)
+      .then((response) => response.json())
+      .then((deets) => {
+        // console.log(deets)
+        setTripDetails(deets);
+      });
+  }
+
+  useEffect(() => {
+    loadTripDetails();
+  }, []);
+
   return (
-    <div>OneTrip</div>
+    <div className='profile'>
+
+      <h1>one trip dhabd;jbhalknfb;fnbalkrnkbajnfblknb;knsbka</h1>
+    </div>
   )
 }
 
