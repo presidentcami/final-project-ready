317555b232314c1d205cb3c26e485c4e2bb8b782
Camille Williams <camiwills325@gmail.com>
2023-05-19 10:32:08 -0400

    Start adding tests

--- client/src/example.test.jsx
+++ client/src/example.test.jsx
@@ -1,18 +1,50 @@
 
-import MyNavBar from './components/Navbar';
-import {expect, test} from 'vitest';
-import {render, screen} from '@testing-library/react';
-import Student from './components/Student';
+import MyNavBar from './routes/Navbar.jsx';
+import {expect, test } from 'vitest';
+import {getByRole, render, screen} from '@testing-library/react';
+import NotLoggedInLandingPage from './components/NotLoggedInLandingPage';
+
+//Navbar rendering
+const user = [{
+  user_auth0_nickname: "camiwills325",
+  user_created: "2023-05-09T21:55:22.122Z",
+  user_email: "camiwills325@gmail.com",
+  user_first_name: "Camille",
+  user_id: 1,
+  user_last_name: "Williams",
+  user_username: null
+}]
+
+// describe('Navbar', () => {
+//   it('renders nav bar', () => {
+//     render(<MyNavBar user={user} />);
+//     expect(
+//       screen.getByTestId('navbar')
+
+//     ).toBeDefined();
+//   })
+// })
+// describe('Login Button', () => {
+//   it('renders login button', () => {
+//     render(<MyNavBar user={user} />);
+//     expect(
+//       screen.getByTestId('navbar')
+
+//     ).toBeDefined();
+//   })
+// })
+
 
 test('Navbar renders correctly', () => {
-  const { getByTestId } = render(<MyNavBar />);
-  const navbarElement = getByTestId('navbar');
+  render(<MyNavBar user={user} />);
+  const navbarElement = screen.getByTestId('navbar');
+  // console.log("navbar element in test jsx", navbarElement)
   expect(navbarElement).toBeDefined();
 });
 
-test("Show full name of a student", () =>{
-  const student = {firstname: "Jair", lastname: "Trejo"};
-  const { getByText } = render(<Student student={student} />);
-  const studentFullName = getByText("Jair Trejo")
-  expect(studentFullName).toBeDefined()
-})
+test('Without user landing page renders correctly', () => {
+  const { getByTestId } = render(<NotLoggedInLandingPage />);
+  const landingPage = getByTestId('landing');
+  expect(landingPage).toBeDefined();
+});
+
