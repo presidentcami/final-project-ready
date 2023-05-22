71f450fd0d42863b4d7a2514a32a286aa550c4f0
Camille Williams <camiwills325@gmail.com>
2023-05-22 17:04:39 -0400

    Update useEffect so page changes when user navigates with sidebar

--- client/src/components/OneTrip.jsx
+++ client/src/components/OneTrip.jsx
@@ -10,22 +10,21 @@ const OneTrip = () => {
   const [tripDetails, setTripDetails] = useState(null)
   const [todos, setTodos] = useState(null);
   // console.log(trip_id)
-
   
 
-  const loadTripDetails = () => {
+  const loadTripDetails = (trip_id) => {
     // A function to fetch the list of students that will be load anytime that list change
     fetch(`http://localhost:8080/tripdetails/${trip_id}`)
       .then((response) => response.json())
       .then((deets) => {
-        console.log(deets)
+        console.log(deets);
         setTripDetails(deets);
       });
-  }
+  };
 
   useEffect(() => {
-    loadTripDetails();
-  }, []);
+    loadTripDetails(trip_id);
+  }, [trip_id]);
 
   /* 
 Trip Details from get request to send as props or map onto screen 
