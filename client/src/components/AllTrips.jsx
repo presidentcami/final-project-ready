0310a84f5c1502b3b078ccd24da2ec02b662e28d
Camille Williams <camiwills325@gmail.com>
2023-05-22 17:05:40 -0400

    Add a conditional statement so if user has no trips, AllTrips tells them to get started

--- client/src/components/AllTrips.jsx
+++ client/src/components/AllTrips.jsx
@@ -5,28 +5,13 @@ import { Link } from 'react-router-dom';
 export default function AllTrips({ user, trips, setTrips }) {
 
   if (!user) return null;
-  const { user_id } = user[0]
-
-  const loadTrips = () => {
-    // A function to fetch the list of students that will be load anytime that list change
-    fetch(`http://localhost:8080/trips/${user_id}`)
-      .then((response) => response.json())
-      .then((trips) => {
-        // console.log(trips)
-        setTrips(trips);
-      });
-  }
-
-  useEffect(() => {
-    loadTrips();
-  }, []);
 
     console.log("trips", trips)
 
   return (
     <div> 
         <div className='profile'>
-              {trips.map((trip) => {
+              {trips.length > 0 ? trips.map((trip) => {
                 return (
                 <li key={trip.trip_id}>
                 <Link to={`${trip.trip_id}`}>{trip.trip_name}</Link>
@@ -35,7 +20,7 @@ export default function AllTrips({ user, trips, setTrips }) {
                 {trip.trip_start_date} - {trip.trip_end_date} */}
                 
                 </li>)
-                })
+                }) : <h3>You don't have any trips yet! Get started here.</h3>
                 }
                 
 
