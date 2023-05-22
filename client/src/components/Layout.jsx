27f3af6e99f9d5089385d6f201f611c3ed5996e8
Camille Williams <camiwills325@gmail.com>
2023-05-22 17:13:06 -0400

    Unsend setTrips as props to sidebar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import MyNavBar from "../routes/Navbar";
 import NotLoggedInLandingPage from "./NotLoggedInLandingPage";
-import SideBar from "./SideBar";
+import SideBar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 
 const Layout = ({ setUser, user, trips, setTrips }) => {
+  console.log(trips)
   return (
     <div className="App">
       <MyNavBar setUser={setUser} user={user} />
 
       {!!user ? (
         <div style={{ flex: true }}>
-          <SideBar trips={trips} setTrips={setTrips} />
+          <SideBar trips={trips} user={user} />
           <Outlet />
         </div>
       ) : (
