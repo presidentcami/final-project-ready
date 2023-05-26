6dc088d907fbe8f12e34d2ed7c880e9388ccc127
Camille Williams <camiwills325@gmail.com>
2023-05-26 14:15:07 -0400

    Clean up unneeded code in Profle.jsx

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,29 +1,10 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import background from '../assets/blue-window.jpg'
-import AddTrip from "./AddNewTrip";
-import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
 
-    const Background = styled.div`
-      background-image: url(${background});
-      min-height: 100vh;
-      background-position: center;
-      background-repeat: no-repeat;
-      background-size: cover;
-    `;
-
-const Profile = ({ user, setTrips }) => {
+const Profile = ({ user }) => {
 
     if (!user) return null;
     const { user_first_name, user_last_name, user_email } = user[0]
-    
-    // console.log("profile", user)
-    // const { isLoading } = useAuth0();
-    
 
-    // if (isLoading) {
-    //     return <div>Loading ...</div>;
-    // }
 
     return user && ( 
         <div className="dashboard"> 
